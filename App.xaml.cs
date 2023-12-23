48415ee41100e024e8bee5ff2d90cd99da7f9c99
XzFg-101 <83053271+XzFg-101@users.noreply.github.com>
2023-12-23 18:08:10 +0400

    Добавлена база данных, а в частности DataBaseService(с моделями)

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Threading.Tasks;
 using System.Windows;
+using SQLite;
 
 namespace AZSProject
 {
@@ -13,5 +17,13 @@ namespace AZSProject
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            IntalizeDataBase();
+        }
+        private void IntalizeDataBase()
+        {
+            
+        } 
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZSProject
+{
+    [Table("User")]
+    public class User
+    {
+        public string PhoneNumber { get; set; }
+        public bool IsClient { get; set; }
+        public string Password { get; set; }
+    }
+}
